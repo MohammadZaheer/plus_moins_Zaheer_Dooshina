393270b4a498f79943dde47284e01f6d300df87c
Zaheer180899 <zaheerariadne@gmail.com>
2020-04-08 21:19:42 +0400

    Commit pour question 6

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void plus_moins(int choix_joueur, int valeur_a_trouver)
 {
     if (choix_joueur < valeur_a_trouver)
     {
-        printf("moins\n");
+        printf("plus\n");
+        printf("La valeur etait %d\n", valeur_a_trouver);
     }
     else if(choix_joueur > valeur_a_trouver)
     {
-        printf("plus\n");
+        printf("moins\n");
+        printf("La valeur etait %d\n", valeur_a_trouver);
     }
     else
     {
@@ -18,11 +22,11 @@ void plus_moins(int choix_joueur, int valeur_a_trouver)
 
 int main()
 {
-    int valeur = 100;
+    srand (time(NULL));
+    int valeur = rand() % 100 + 1;
     int choix;
     printf("Entrez une entier svp: ");
     scanf("%d", &choix);
     plus_moins(choix, valeur);
-
     return 0;
 }
