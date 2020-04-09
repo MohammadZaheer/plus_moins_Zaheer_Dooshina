fa0c89117f9781cd1eda448b291cc3bb1d84dfee
Zaheer180899 <zaheerariadne@gmail.com>
2020-04-09 21:43:17 +0400

    Commit pour question 7

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -4,20 +4,20 @@
 
 void plus_moins(int choix_joueur, int valeur_a_trouver)
 {
-    if (choix_joueur < valeur_a_trouver)
+    do
     {
-        printf("plus\n");
-        printf("La valeur etait %d\n", valeur_a_trouver);
-    }
-    else if(choix_joueur > valeur_a_trouver)
-    {
-        printf("moins\n");
-        printf("La valeur etait %d\n", valeur_a_trouver);
-    }
-    else
-    {
-        printf("Bingo\n");
-    }
+        if (choix_joueur < valeur_a_trouver)
+        {
+            printf("plus\n");
+        }
+        else if(choix_joueur > valeur_a_trouver)
+        {
+            printf("moins\n");
+        }
+        printf("Entrez une entier svp: ");
+        scanf("%d", &choix_joueur);
+    } while (choix_joueur != valeur_a_trouver);
+    printf("Bingo\n");
 }
 
 int main()
